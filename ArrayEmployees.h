b680e7ead15c9decc8a3fe635db05834084aa2bf
Valeria Gil <60628850+saakura14@users.noreply.github.com>
2020-05-15 15:15:17 -0300

    Correcciones

--- ArrayEmployees.h
+++ ArrayEmployees.h
@@ -23,46 +23,52 @@ typedef struct
  */
 int initEmployees(eEmployee list[], int len) ;
 
-/** \brief add in a existing list of employees the values received as parameters
- * in the first empty position
- * \param list employee
- * \param len int
- * \param int flag to know if an employees it's created
- * \return  int Return (-1) if Error [Invalid length or NULL pointer or without
- * free space] - (0) if Ok
+/** \brief To indicate that all position in the array are empty,
+* this function put the flag (isEmpty) in TRUE in all
+* position of the array
+* \param list Employee* Pointer to array of employees
+* \param len int Array length
+* \return int Return (-1) if Error [Invalid length or NULL pointer] - (0) if Ok
+*
 */
 int addEmployee(eEmployee list[], int len) ;
 
+/** \brief add in a existing list of employees the values received as parameters
+* in the first empty position
+* \param list employee*
+* \param len int
+* \param id int
+* \param name[] char
+* \param lastName[] char
+* \param salary float
+* \param sector int
+* \return int Return (-1) if Error [Invalid length or NULL pointer or without
+free space] - (0) if Ok */
 
-/** \brief  find an Employee by Id en returns the index position in array.
- *
- * \param list employee
- * \param len int
- * \param id int
- * \return  Return employee index position or (-1) if the employee not found.
- *
- */
 int findEmployeeById(eEmployee list[], int len, int id) ;
 
-/** \brief Remove a Employee by Id (put isEmpty Flag in 1)
- *
- * \param list Employee
- * \param len int
- * \param id int
- * \return int Return (-1) if Error [Invalid length or NULL pointer or if can't
-find a employee] - (0) if Ok
- *
- */
+/** \brief find an Employee by Id en returns the index position in array.
+*
+* \param list Employee*
+* \param len int
+* \param id int
+* \return Return employee index position or (-1) if [Invalid length or NULL
+pointer received or employee not found]
+*
+*/
 int removeEmployee(eEmployee list[], int len, int id) ;
 
 
-/** \brief Busca un indice libre
- *
- * \param Array a buscar.
- * \param Tamaño del array
- * \return Devuelve el indice dond eesta vacio, y si no lo esta devuelve -1.
- *
- */
+/** \brief Remove a Employee by Id (put isEmpty Flag in 1)
+*
+* \param list Employee*
+* \param len int
+* \param id int
+* \return int Return (-1) if Error [Invalid length or NULL pointer or if can't
+find a employee] - (0) if Ok
+*
+*/
+
 int searchEmpty(eEmployee lista[],int len) ;
 
 /** \brief Modifica un emplado cargado
@@ -99,13 +105,15 @@ indicate UP or DOWN order
  */
 int sortEmployees(eEmployee list[], int len, int order);
 
-/** \brief Cuenta cuantos empleados estan por encima del salario promedio
- *
- * \param array de empleados a verificar
- * \param tamaño del array
- * \return int cantidad de empleados
- *
- */
+/** \brief Sort the elements in the array of employees, the argument order
+indicate UP or DOWN order
+*
+* \param list Employee*
+* \param len int
+* \param order int [1] indicate UP - [0] indicate DOWN
+* \return int Return (-1) if Error [Invalid length or NULL pointer] - (0) if Ok
+*
+*/
 int countOverAverage(eEmployee list[],int len);
 
 /** \brief Calcula el promedio de los salarios del array
@@ -135,7 +143,7 @@ float totalSalary(eEmployee list[],int len) ;
  * \return TRUE si hay un empleado o FALSE si no hay un empleado
  *
  */
-int yaHayEmpleado(eEmployee list[],int len);
+int addEmployee(eEmployee list[],int len);
 
 
 
